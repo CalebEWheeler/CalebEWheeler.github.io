e90c3c3d2ec6a21779dece1475a54fe7b16fc8a7
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-26 17:12:24 -0500

    feat: have a functional scrollTo() for CONTACT tab for smaller screen size

--- index.js
+++ index.js
@@ -27,21 +27,18 @@ $(document).ready(function () {
         e.preventDefault();
         switch ($(this).data("id")) {
             case 1:
-                console.log("home");
                 $(this).parent().parent().parent().parent().parent().animate({
                     scrollLeft: 0,
                 }, 800, function () {
                 });
                 break;
             case 2:
-                console.log("about");
                 $(this).parent().parent().parent().parent().parent().animate({
                     scrollLeft: 767,
                 }, 800, function () {
                 });
                 break;
             case 3:
-                console.log("skills");
                 $(this).parent().parent().parent().parent().parent().animate({
                     scrollLeft: 1534,
                 }, 800, function () {
@@ -55,34 +52,31 @@ $(document).ready(function () {
                 });
                 break;
             case 5:
-                console.log("education");
                 $(this).parent().parent().parent().parent().parent().animate({
                     scrollLeft: 3068,
                 }, 800, function () {
                 });
                 break;
             case 6:
-                console.log("recommendations");
                 $(this).parent().parent().parent().parent().parent().animate({
                     scrollLeft: 3835,
                 }, 800, function () {
                 });
                 break;
             case 7:
-                console.log("portfolio");
                 $(this).parent().parent().parent().parent().parent().animate({
                     scrollLeft: 4602,
                 }, 800, function () {
                 });
                 break;
             case 8:
-                console.log("contact");
                 $(this).parent().parent().parent().parent().parent().animate({
-                    scrollLeft: 4608,
+                    scrollLeft: 5150,
                 }, 800, function () {
                 });
                 break;
         }
     })
+
 
 })
