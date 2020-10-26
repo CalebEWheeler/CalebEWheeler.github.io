c40adc9434eed763ce41b51612aaba592c15453e
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-26 08:56:47 -0500

    feat: apply  smooth scroll functionality for navbar tabs to my switch case in index.js

--- index.js
+++ index.js
@@ -1,63 +1,86 @@
-
 $(document).ready(function () {
     //!WHEN ICON IS CLICKED REDIRECTS TO TOP OF PAGE!
-    $('#icon').click(function(e) {
+    $('#icon').click(function (e) {
         e.preventDefault()
     })
 
     $('#myNavItems').hide();
-    $('.link').click(function() {
+    $('.link').click(function () {
         $('#myNavItems').slideToggle();
     })
 
     $('.education').hover(
-        function() {
+        function () {
             $(this).find('i').removeClass('fa-3x').addClass('fa-4x')
-    },  function() {
+        }, function () {
             $(this).find('i').removeClass('fa-4x').addClass('fa-3x')
         })
 
-    $('.toTopOfPage').click(function() {
+    $('.toTopOfPage').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, "slow");
         return false;
     })
 
-    $('.lgNavbar').children('.link1').click(function(e) {
+    $('.lgNavbar').children('.link1').click(function (e) {
         e.preventDefault();
-        switch($(this).data("id")) {
+        switch ($(this).data("id")) {
             case 1:
                 console.log("home");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(0);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 0,
+                }, 800, function () {
+                });
                 break;
             case 2:
                 console.log("about");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(767);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 767,
+                }, 800, function () {
+                });
                 break;
             case 3:
                 console.log("skills");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(1534);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 1534,
+                }, 800, function () {
+                });
                 break;
             case 4:
                 console.log("experience");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(2301);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 2301,
+                }, 800, function () {
+                });
                 break;
             case 5:
                 console.log("education");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(3068);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 3068,
+                }, 800, function () {
+                });
                 break;
             case 6:
                 console.log("recommendations");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(3835);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 3835,
+                }, 800, function () {
+                });
                 break;
             case 7:
                 console.log("portfolio");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(4602);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 4602,
+                }, 800, function () {
+                });
                 break;
             case 8:
                 console.log("contact");
-                $(this).parent().parent().parent().parent().parent().scrollLeft(4608);
+                $(this).parent().parent().parent().parent().parent().animate({
+                    scrollLeft: 4608,
+                }, 800, function () {
+                });
                 break;
         }
     })
